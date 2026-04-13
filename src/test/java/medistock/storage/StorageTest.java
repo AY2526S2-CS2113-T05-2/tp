e767065af9274e15ac441951f2ae63bd9ca4e504
podles <podlevan@gmail.com>
2026-04-14 04:42:10 +0800

    add new line

--- src/test/java/medistock/storage/StorageTest.java
+++ src/test/java/medistock/storage/StorageTest.java
@@ -14,7 +14,10 @@ import java.nio.file.Path;
 import java.time.LocalDate;
 import java.util.Collection;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class StorageTest {
 
